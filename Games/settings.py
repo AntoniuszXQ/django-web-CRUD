1c66795e9424e1033ec521a32c6b2a0634ec6590
AntoniuszXQ <76694780+AntoniuszXQ@users.noreply.github.com>
2021-12-21 11:02:13 +0100

    Update settings.py

--- Games/settings.py
+++ Games/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$h_oj0%#jc^)!evbboj3)#yyjm1*u%m5w@ul=ha@74ntdrk(0v'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
